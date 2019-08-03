8c6b7ca7785041255d6484074e06417bd46bd650
Deborah Savage <savaged725@yahoo.com>
2019-08-03 01:09:29 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,12 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+def display_board 
+board = [" "," "," "," "," "," "," ", " ", " "]
+end 
+def display_board(board)
+row = ("   |   |   ")
+row = ("-----------")
+row = ("   |   |   ")
+row = ("-----------")
+row = ("   |   |   ")
+end
+def display_board(board)
+  puts "#{board[0]}"
