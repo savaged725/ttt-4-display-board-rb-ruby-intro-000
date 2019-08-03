860e1cee227b154e9856b6e0a8957f79d674f401
Deborah Savage <savaged725@yahoo.com>
2019-08-03 01:12:26 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,12 +1,7 @@
-def display_board 
-board = [" "," "," "," "," "," "," ", " ", " "]
-end 
-def display_board(board)
-row = ("   |   |   ")
-row = ("-----------")
-row = ("   |   |   ")
-row = ("-----------")
-row = ("   |   |   ")
-end
-def display_board(board)
-  puts "#{board[0]}"
+def display_board
+   puts row = ["   " "|" "   " "|" "   "]
+   puts separator = "-----------"
+   puts row
+   puts separator
+   puts row
+ end
